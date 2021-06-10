73af09bb0a7e6a2956b7f0fb313cc448d5b422c1
SooHyunKSH <82920745+SooHyunKSH@users.noreply.github.com>
2021-06-10 09:32:41 +0900

    ❤️‍🔥포트폴리오 추가

--- js/common.js
+++ js/common.js
@@ -3,9 +3,47 @@ $(document).ready(function() {
 		//options here
 		autoScrolling:true,
 		scrollHorizontally: true,
-        menu: '#menu',
+        menu: '#gnb',
+		lockAnchors: false,
+		responsiveWidth: 415,
+        anchors:['welcome','work','profile','comtect'],
+		onLeave: function (origin, destination, direction) {
+            if (destination > 1) {
+                $("#header").addClass("on");
+				$(".gnb_btn").css("display","block");
+            }else{
+				$("#header").removeClass("on");
+				$(".gnb_btn").css("display","none");
+			}
+        }
 	});
 
 	//methods
 	$.fn.fullpage.setAllowScrolling(true);
+
+	$(".gnb_btn").click(function(){
+		$("#header").css("right","40rem");
+		$(".gnb_btn").css("display","none");
+		console.log("text")
+	});
+	$(".gnb_btn_close").click(function(){
+		$("#header").css("right","-40rem");
+		$(".gnb_btn").css("display","block");
+	});
+
+	/* portfolio 클릭 */
+	$(".site_box").click(function(){
+		let siteBoxIndex = $(this).index() + 1;
+		$(".work_wrap").removeClass('on')
+		$(".work_wrap").each(function(){
+			let workWrapIndex = $(this).index();
+			if(siteBoxIndex == workWrapIndex){
+				$(this).addClass('on');
+			}
+		})
+	})
+	// close버튼 클릭 시 work_wrap닫기
+	$(".close_btn").click(function(){
+		$(".work_wrap").removeClass('on')
+	})
 });
